85524d73df867286bb3eabb2353c2a2f94bc5f44
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-11-22 12:24:21 +0530

    We have changed the AMI Details

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ resource "aws_lb" "main" {
   name               = local.load_balancer_name
   internal           = var.internal
   load_balancer_type = var.load_balancer_type
-  security_groups    = [aws_security_group.lb_sg.id]
+  security_groups = [aws_security_group.lb_sg.id]
   subnets            = var.subnets
-  tags               = merge(local.tags, { Name = local.load_balancer_name })
+  tags = merge(local.tags, { Name = local.load_balancer_name })
 }
 
 
@@ -14,15 +14,15 @@ resource "aws_security_group" "lb_sg" {
   name        = local.sg_name
   description = local.sg_name
   vpc_id      = var.vpc_id
-  tags        = merge(var.tags, { Name = local.sg_name })
+  tags = merge(var.tags, { Name = local.sg_name })
 }
 
 
 # Ingress rule for ALB SG.
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  for_each          = toset(var.cidr_block) # Convert list to a set to iterate over each CIDR
+  for_each = toset(var.cidr_block) # Convert list to a set to iterate over each CIDR
   security_group_id = aws_security_group.lb_sg.id
-  cidr_ipv4         = each.value  # Each CIDR block as a separate rule
+  cidr_ipv4 = each.value  # Each CIDR block as a separate rule
   from_port         = var.sg_port
   ip_protocol       = "tcp"
   to_port           = var.sg_port
@@ -33,11 +33,11 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
 resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   count             = var.internal ? 0 : 1
   security_group_id = aws_security_group.lb_sg.id
-  cidr_ipv4         = "0.0.0.0/0"  # Each CIDR block as a separate rule
+  cidr_ipv4 = "0.0.0.0/0"  # Each CIDR block as a separate rule
   from_port         = 80
   to_port           = 80
   ip_protocol       = "tcp"
-  tags              = { Name = "User-to-Application" }
+  tags = { Name = "User-to-Application" }
 }
 
 
